f7caa9ba33f1316d5b82c05d913c0b6d3074afe9
killje <patrick.beuks@home.nl>
2014-05-23 09:35:36 +0200

    made it kinda work?

--- nbhaskell/src/org/hoovooloo/HaskellDataObject.java
+++ nbhaskell/src/org/hoovooloo/HaskellDataObject.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.hoovooloo;
 
 import java.io.IOException;
@@ -14,6 +10,7 @@ import org.openide.nodes.CookieSet;
 import org.openide.nodes.Node;
 import org.openide.nodes.Children;
 import org.openide.util.Lookup;
+
 import org.openide.text.DataEditorSupport;
 
 public class HaskellDataObject extends MultiDataObject {
